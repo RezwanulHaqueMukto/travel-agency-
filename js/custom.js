3b84d3c8b8b820035b2dc5459e553adec71b78f6
REZWANUL1 <74784389+REZWANUL1@users.noreply.github.com>
2022-08-08 20:17:21 +0600

    add flight bottom

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,47 @@
-let nav_a = document.querySelectorAll("nav ul li a");
-let nav_links = document.querySelectorAll("nav ul li");
-let nav_container = document.querySelector(".nav-container");
-let nav_toggle = document.querySelector("#navToggle");
-let header = document.querySelector(".header");
+// let nav_a = document.querySelectorAll("nav ul li a");
+// let nav_links = document.querySelectorAll("nav ul li");
+// let nav_container = document.querySelector(".nav-container");
+// let nav_toggle = document.querySelector("#navToggle");
+// let header = document.querySelector(".header");
 
-//?#####NOTE:adding  active class in nav links at main navbar####
-//? #######################
+// //?#####NOTE:adding  active class in nav links at main navbar####
+// //? #######################
 
-nav_a.forEach((link) => {
-	link.addEventListener("click", function () {
-		nav_a.forEach((link_remove) => {
-			link_remove.classList.remove("active");
-		});
-		link.classList.add("active");
-	});
-});
-nav_links.forEach((element) => {
-	element.addEventListener("click", function () {
-		nav_container.classList.add("active");
-	});
-});
+// nav_a.forEach((link) => {
+// 	link.addEventListener("click", function () {
+// 		nav_a.forEach((link_remove) => {
+// 			link_remove.classList.remove("active");
+// 		});
+// 		link.classList.add("active");
+// 	});
+// });
+// nav_links.forEach((element) => {
+// 	element.addEventListener("click", function () {
+// 		nav_container.classList.add("active");
+// 	});
+// });
 
-//?#####NOTE:toggle active in nav-container for bring mobile menu####
-//? #######################
+// //?#####NOTE:toggle active in nav-container for bring mobile menu####
+// //? #######################
 
-nav_toggle.addEventListener("click", function () {
-	nav_container.classList.toggle("active");
-});
-//?#######NOTE:sticky menu
-//? #######################
-let topOfNav = nav_container.offsetTop;
+// nav_toggle.addEventListener("click", function () {
+// 	nav_container.classList.toggle("active");
+// });
+// //?#######NOTE:sticky menu
+// //? #######################
+// let topOfNav = nav_container.offsetTop;
 
-console.log(topOfNav);
-function fixedNav() {
-	// console.log(navHeight);
+// console.log(topOfNav);
+// function fixedNav() {
+// 	// console.log(navHeight);
 
-	if (window.scrollY > topOfNav) {
-		
-		header.classList.add("headerBackground");
-	} else {
-		
-		header.classList.remove("headerBackground");
-	}
-}
-window.addEventListener("scroll", fixedNav);
+// 	if (window.scrollY > topOfNav) {
+
+// 		header.classList.add("headerBackground");
+// 	} else {
+
+// 		header.classList.remove("headerBackground");
+// 	}
+// }
+// window.addEventListener("scroll", fixedNav);
+
