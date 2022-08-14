8ef1247b42eba30b5dc9d744526768e7b4503366
REZWANUL1 <74784389+REZWANUL1@users.noreply.github.com>
2022-08-14 23:58:05 +0600

    add active class navbar

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,25 @@
-// let nav_a = document.querySelectorAll("nav ul li a");
-// let nav_links = document.querySelectorAll("nav ul li");
-// let nav_container = document.querySelector(".nav-container");
-// let nav_toggle = document.querySelector("#navToggle");
-// let header = document.querySelector(".header");
+let nav_a = document.querySelectorAll("nav ul li a");
+let nav_links = document.querySelectorAll("nav ul li");
+let nav_container = document.querySelector(".nav-container");
+let nav_toggle = document.querySelector("#navToggle");
+let header = document.querySelector(".header");
 
 // //?#####NOTE:adding  active class in nav links at main navbar####
 // //? #######################
 
-// nav_a.forEach((link) => {
-// 	link.addEventListener("click", function () {
-// 		nav_a.forEach((link_remove) => {
-// 			link_remove.classList.remove("active");
-// 		});
-// 		link.classList.add("active");
-// 	});
-// });
-// nav_links.forEach((element) => {
-// 	element.addEventListener("click", function () {
-// 		nav_container.classList.add("active");
-// 	});
-// });
+nav_a.forEach((link) => {
+	link.addEventListener("click", function () {
+		nav_a.forEach((link_remove) => {
+			link_remove.classList.remove("active");
+		});
+		link.classList.add("active");
+	});
+});
+nav_links.forEach((element) => {
+	element.addEventListener("click", function () {
+		nav_container.classList.add("active");
+	});
+});
 
 // //?#####NOTE:toggle active in nav-container for bring mobile menu####
 // //? #######################
